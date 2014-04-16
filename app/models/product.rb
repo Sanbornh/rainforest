55d845bb977b7cf04fda546ebcec71ad33d5e707
Sanborn <sanbornh@rogers.com>
2014-04-16 14:01:27 -0400

    Moved price formatting out of view into product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ActiveRecord::Base
 	validates :description, :name, :presence => true
 	# validates :price_in_cents, :numericality => {:only_float => true}
 
+	def formatted_price
+		sprintf "%.2f", (@product.price_in_cents * 0.01).round(2)
+	end
 end
